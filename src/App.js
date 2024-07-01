4a258bf998107919c342aa291118699b91c035e6
yashchoraria <yc21chb0a60@student.nitw.ac.in>
2024-07-01 23:52:21 +0530

    the basic outlook is ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import Details from './components/Details'
-import { Grid } from '@material-ui/core'
+import React from 'react';
+import Details from './components/Details/Details';
+import { Grid } from '@material-ui/core';
+import useStyles from './styles';
+import Main from './components/Details/Main/Main';
 
 const App = () => {
-  return (
-    <div>
-        <Grid container spacing={0} alignItems="center" justifyContent="center" style={{ height: '100vh' }}>
-            <Grid item xs={12} sm={4}>
-                <Details />
+    const classes = useStyles();
+    return (
+        <div>
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justifyContent="center" style={{ height: '100vh' }}>
+                <Grid item xs={12} sm={4}>
+                    <Details title="Income" />
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                    <Main />   
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details title="Expense" />
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-                Main    
-            </Grid>
-            <Grid item xs={12} sm={4}>
-                <Details />
-            </Grid>
-        </Grid>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default App
